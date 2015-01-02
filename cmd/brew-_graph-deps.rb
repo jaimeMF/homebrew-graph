1ea011063911f18f130255031d2a3b5a4f865458
Jaime Marquínez Ferrándiz <jaime.marquinez.ferrandiz@gmail.com>
2015-01-02 18:27:44 +0100

    Use a custom script for getting the dependencies
    It takes into account the build options for installed formulae

---
+++ cmd/brew-_graph-deps.rb
@@ -0,0 +1,40 @@
+# The script is adapted from the Homebrew core 'deps' command
+require 'formula'
+require 'ostruct'
+
+def deps_for_formula(f, installed)
+  if installed
+    deps = f.deps.select do |dep|
+      # This is how `brew missing` detects the dependencies for an installed formula
+      if dep.optional? || dep.recommended?
+        Tab.for_formula(f).with?(dep)
+      else
+        # build dependencies are listed, but they are not needed for using the formula
+        # TODO: think about not listing them
+        true
+      end
+    end
+    dseps = deps.to_a
+  else
+    deps = f.deps.default
+  end
+
+  reqs = f.requirements
+
+  deps + reqs.select(&:default_formula?).map(&:to_dependency)
+end
+
+def puts_deps(formulae, installed)
+  formulae.each { |f| puts "#{f.name}: #{deps_for_formula(f, installed) * " "}" }
+end
+
+mode = OpenStruct.new(
+  :installed?  => ARGV.include?('--installed'),
+  :all?        => ARGV.include?('--all'),
+)
+
+if mode.installed?
+  puts_deps Formula.installed, true
+elsif mode.all?
+  puts_deps Formula, false
+end
